66f0b2f5c43bfef1827a57593a91da10039be224
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2017-08-17 16:13:05 +0200

    Add properties for max_content_length_validation

--- utf8field/fields.py
+++ utf8field/fields.py
@@ -9,6 +9,17 @@ from utf8field import forms
 class UTF8FileField(models.FileField):
     description = _('A text file containing only UTF-8 text')
 
+    def __init__(self, max_content_length=None, *args, **kwargs):
+        self.max_content_length = max_content_length
+        super(UTF8FileField, self).__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(UTF8FileField, self).deconstruct()
+
+        if self.max_content_length:
+            kwargs['max_content_length'] = self.max_content_length
+        return name, path, args, kwargs
+
     def formfield(self, **kwargs):
         defaults = {'form_class': forms.UTF8FileField}
         defaults.update(kwargs)
