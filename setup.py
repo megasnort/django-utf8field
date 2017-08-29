339cc453192eaf2c0568b315483bb354f8e47d50
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2017-08-29 13:29:21 +0200

    Change version to v0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='django-utf8field',
-      version='0.0.2',
+      version='0.1.0',
       description='Add UTF-8 Validation to a Django FileField',
       long_description=readme(),
       classifiers=[
@@ -21,7 +21,7 @@ setup(name='django-utf8field',
       author='Stef Bastiaansen',
       author_email='stef@megasnort.com',
       url='https://github.com/megasnort/django-utf8field',
-      download_url='https://github.com/megasnort/django-utf8field/archive/v0.0.2.tar.gz',
+      download_url='https://github.com/megasnort/django-utf8field/archive/v0.1.0.tar.gz',
       packages=['utf8field'],
       keywords='django utf-8',
       license='Apache',
