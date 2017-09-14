e3198831e2f5d5574b373edd21d286c956545a92
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2017-09-14 12:14:57 +0200

    When djangorestframework is not installed, the package should still be usable

--- utf8field/__init__.py
+++ utf8field/__init__.py
@@ -1,7 +1,11 @@
-from utf8field.fields import UTF8TextField, UTF8CharField, UTF8FileField
-from utf8field.serializer_fields import UTF8TextSerializerField, UTF8CharSerializerField, UTF8FileSerializerField
-from rest_framework.serializers import ModelSerializer
+try:
+    from rest_framework.serializers import ModelSerializer
+    from utf8field.fields import UTF8TextField, UTF8CharField, UTF8FileField
+    from utf8field.serializer_fields import UTF8TextSerializerField, UTF8CharSerializerField, UTF8FileSerializerField
 
-ModelSerializer.serializer_field_mapping[UTF8TextField] = UTF8TextSerializerField
-ModelSerializer.serializer_field_mapping[UTF8CharField] = UTF8CharSerializerField
-ModelSerializer.serializer_field_mapping[UTF8FileField] = UTF8FileSerializerField
+    ModelSerializer.serializer_field_mapping[UTF8TextField] = UTF8TextSerializerField
+    ModelSerializer.serializer_field_mapping[UTF8CharField] = UTF8CharSerializerField
+    ModelSerializer.serializer_field_mapping[UTF8FileField] = UTF8FileSerializerField
+except ImportError, e:
+    # well, we won't be needing the restframework anyway
+    pass
